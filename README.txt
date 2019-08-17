8eb8dcaaae40ab8e90ad308338eb840f863c841f
CleoCola <cl3ocola@gmail.com>
2019-08-17 14:37:21 -0500

    initial commit, html and js

---
+++ README.txt
@@ -0,0 +1,6 @@
+Fourth installment of at home assignments for UT coding boot camp.
+
+Assignment is to crate an html based random-number generating game.
+Game uses pictures of crystals as buttons that increments users score.
+Objective is to match the random number that the site creates.
+Functions created using JavaScript and JQuery.
---
+++ assets/js/game.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+	let Random=Math.floor(Math.random()*19+101)
+	$('#randomNumber').text(Random);
+	let greenNumber=Math.floor(Math.random()*1+11)
+	let redNumber=Math.floor(Math.random()*1+11)
+	let blueNumber=Math.floor(Math.random()*1+11)
+	let yellowNumber=Math.floor(Math.random()*1+11)
+	let score=0;
+	let wins=0;
+	let losses=0;
+
+	$('#numberOfWins').text(wins);
+	$('#numberOfLosses').text(losses);
+
+    $('#green').on('click', function(){
+		score=score + num4;
+		console.log("New score=" + score);
+        $('#totalScore').text(score);
+        
+			if(score === Random){
+				win();
+			}
+			else if(score > Random){
+				loss();
+			}
+    })
+    
+	$('#red').on('click', function(){
+		score=score + redNumber;
+		console.log("New score=" + score);
+        $('#totalScore').text(score);
+        
+			if(score === Random){
+				win();
+			}
+			else if(score > Random){
+				loss();
+			}
+	})
+
+	$('#blue').on('click', function(){
+		score=score + blueNumber;
+        $('#totalScore').text(score);
+        
+			if(score === Random){
+				win();
+			}
+			else if(score > Random){
+				loss();
+			}
+	})
+
+	$('#yellow').on('click', function(){
+		score=score + yellowNumber;
+        $('#totalScore').text(score);
+        
+			if(score === Random){
+				win();
+			}
+			else if(score > Random){
+				loss();
+			}
+	})
+
+	function win(){
+		alert("Success!");
+	 	wins++; 
+	  	$('#numberOfWins').text(wins);
+	  	reset();
+	}
+
+	function loss(){
+		alert("Try again.");
+		losses++;
+		$('#numberOfLosses').text(losses);
+		reset();
+	}
+
+	function reset(){
+		Random=Math.floor(Math.random()*101+19);
+		$('#randomNumber').text(Random);
+		greenNumber=Math.floor(Math.random()*11+1);
+		redNumber=Math.floor(Math.random()*11+1);
+		blueNumber=Math.floor(Math.random()*11+1);
+		yellowNumber=Math.floor(Math.random()*11+1);
+		score=0;
+		$('#totalScore').text(score);
+	}
+
+});
